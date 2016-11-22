d5b6b6d1b6c96e4108325407015d7922e0e3d74d
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-11-21 19:40:08 -0500

    Created folders and files for front end service and linked to index.html

---
+++ public/js/controllers/main.controller.js
@@ -0,0 +1,12 @@
+(function() {
+
+  angular.module('two-cents')
+         .controller('main-controller', MainController);
+
+  MainController.$inject = ['$scope'];
+
+  function MainController($scope){
+    
+  }
+
+}());
