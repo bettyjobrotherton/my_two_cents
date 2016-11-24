aec641742776d78794e7a036cb9f37150b526492
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-11-23 19:57:21 -0500

    Created user router skeleton and fleshed out get by id route

---
+++ server/routers/user.router.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../models/user.model.js');
+
+router.get('/users/profile/:userID', function(req, res){
+  User.find({_id: req.params.userID}, function(err, user){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+  });
+});
+
+router.post('/users/signup', function(req, res){});
+
+router.post('/users/login', function(req,res){});
+
+router.put('/users/profile/:userID', function(req, res){});
+
+module.exports = router;
