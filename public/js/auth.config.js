62da7eece87feb0aa95f82b5d712249e082b3cd1
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-07 18:59:20 -0500

    Protected angular routes that require login

--- public/js/auth.config.js
+++ public/js/auth.config.js
@@ -2,7 +2,14 @@
   angular.module('two-cents')
     .run(AuthConfig);
 
-  AuthConfig.$inject = [];
+  AuthConfig.$inject = ['$rootScope', '$location', 'AuthService'];
 
-  function AuthConfig(){}
+  function AuthConfig($rootScope, $location, AuthService){
+    $rootScope.$on('$routeChangeStart', function(event, nextRoute, currentRoute){
+      if(nextRoute.access.restricted && !AuthService.isLoggedIn()){
+        $location.path('/'); //sends user to the home page if they aren't logged in and try to go to a protected route
+        
+      }
+    });
+  }
 }());
