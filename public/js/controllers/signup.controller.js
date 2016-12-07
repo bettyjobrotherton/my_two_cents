18c4c6c39ecf561f2179ff09bc5583660e9fd09a
Elizabeth Brotherton <elizabeth.j.brotherton@gmail.com>
2016-12-06 19:33:25 -0500

    Implemented sign up view with basic visualization and error handling

--- public/js/controllers/signup.controller.js
+++ public/js/controllers/signup.controller.js
@@ -2,16 +2,16 @@
   angular.module('two-cents')
     .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = ['$scope', 'AuthService'];
+  SignUpController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function SignUpController($scope, AuthService){
+  function SignUpController($scope, AuthService, $location){
     $scope.signup = signup;
     $scope.required = true;
 
     function signup(newUser){
       AuthService.signup(newUser)
                  .then(function(){
-                   alert('Yayyy!!! Signing up!');
+                   $location.path('/login');
                  })
                  .catch(function(){
                    $scope.newUser = {};
